b07dd9de0d3048f8aeab17f006c60dcfc22684d9
Sambhram <Sambhrammathad@gmail.com>
2024-02-22 23:24:09 +0530

    check

--- projectcart/settings.py
+++ projectcart/settings.py
@@ -18,11 +18,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
